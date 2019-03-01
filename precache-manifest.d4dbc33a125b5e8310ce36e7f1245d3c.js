251cbf20f9ac16ed7fdceca7bd6631d21c308b0f
Jmay <justin.sean.may@gmail.com>
2019-02-28 22:42:12 -0500

    Updates

---
+++ precache-manifest.d4dbc33a125b5e8310ce36e7f1245d3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2d778eddf49f489dfb6d",
+    "url": "/static/js/main.2d778edd.chunk.js"
+  },
+  {
+    "revision": "801674942fa32f89c3dc",
+    "url": "/static/js/2.80167494.chunk.js"
+  },
+  {
+    "revision": "2d778eddf49f489dfb6d",
+    "url": "/static/css/main.ce978d28.chunk.css"
+  },
+  {
+    "revision": "74da211238a797850bea11e6ed6a8bb3",
+    "url": "/index.html"
+  }
+];
